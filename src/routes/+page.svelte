e730fb747710ab012b7aa0522ee70dce5d6a9e9c
matthew lee <laughew@gmail.com>
2023-07-14 18:04:15 -0700

    entirely too much css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,35 @@
 <script>
 	import sky from '$lib/images/sky.jpg';
 	import grass from '$lib/images/grass.png';
+	let y = 0;
+	let h = 0;
+	$: hStart = h - 150;
 </script>
 
+<svelte:window bind:scrollY={y} bind:innerHeight={h} />
 <div class="hero-wrapper">
 	<div class="sky-wrapper">
 		<img class="sky hero" alt="sky banner" src={sky} />
 		<h5 class="hero-text intro">you are cordially invited to...</h5>
 	</div>
 	<div class="title-wrapper">
-		<h5 class="hero-text title matthew">Matthew’s</h5>
-		<h1 class="hero-text title birthday">Birthday</h1>
-		<h5 class="hero-text title in-the">at the</h5>
-		<h1 class="hero-text title park">Park</h1>
+		<h5 class:spaced={y > hStart && y < hStart + 120} class="hero-text title matthew">Matthew’s</h5>
+		<div class="hero-text title birthday">
+			<span class:jump={y > hStart + 40 && y < hStart + 140}>B</span>
+			<span class:jump={y > hStart + 60 && y < hStart + 160}>i</span>
+			<span class:jump={y > hStart + 80 && y < hStart + 180}>r</span>
+			<span class:jump={y > hStart + 100 && y < hStart + 200}>t</span>
+			<span class:jump={y > hStart + 120 && y < hStart + 220}>h</span>
+			<span class:jump={y > hStart + 140 && y < hStart + 240}>d</span>
+			<span class:jump={y > hStart + 160 && y < hStart + 260}>a</span>
+			<span class:jump={y > hStart + 180 && y < hStart + 280}>y</span>
+		</div>
+		<h5 class:wobble={y > hStart + 280 && y < hStart + 380} class="hero-text title in-the">
+			at the
+		</h5>
+		<h1 class:grow={y > hStart + 400 && y < hStart + 500} class="hero-text title park">Park</h1>
 	</div>
-	<div class="spacer">
-	</div>
+	<div class="spacer" />
 	<div class="grass-wrapper">
 		<img class="grass hero" alt="grass banner" src={grass} />
 	</div>
@@ -86,7 +100,7 @@
 		top: 25vh;
 		margin-bottom: -100vh;
 		max-width: 100%;
-		padding: 2rem;
+		padding: 1.6rem;
 		overflow: hidden;
 	}
 
@@ -94,32 +108,59 @@
 		font-family: 'Orange Squash';
 		letter-spacing: 1px;
 		position: relative;
+		transition: letter-spacing 0.4s ease-in-out;
 		margin: 0;
 	}
 
 	.matthew {
 		font-size: 30px;
-		font-family: 'Nanum Myeongjo'
+		font-family: 'Nanum Myeongjo';
 	}
 
 	.birthday {
+		font-size: 0px;
+	}
+
+	.birthday span {
 		font-size: 80px;
+		margin: 0 -1px;
+		display: inline-block;
+		transition: translate 0.1s ease-in-out;
 	}
 
 	.in-the {
-		margin-top: -16px;
+		margin-top: -18px;
 		font-family: 'Nanum Myeongjo';
 		margin-bottom: -10px;
-	}
-	
-	.park {
-		font-size: 60px;
+		transform-origin: top left;
+		transition: rotate 0.4s ease-in-out;
 	}
 
+	.park {
+		font-size: 60px;
+		transition: scale 0.2s ease-out;
+		transform-origin: top left;
+	}
+
+	.spaced {
+		letter-spacing: 4px;
+	}
+
+	.jump {
+		translate: 0 -15px;
+	}
+
+	.wobble {
+		rotate: -20deg;
+	}
+
+	.grow {
+		scale: 1.2;
+	}
 	.spacer {
 		height: 200vh;
 	}
-	
+
 	.grass {
 		position: relative;
 		bottom: -10px;
